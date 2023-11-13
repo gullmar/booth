5976219ccc2ed1102c69926c1efbfe560d761175
Marco Gullo <marco.gullo@mailbox.org>
2023-11-14 00:34:06 +0100

    refactor db and add logs

--- booth/db.py
+++ booth/db.py
@@ -1,5 +1,7 @@
 import sqlite3
+from typing import Optional
 from uuid import uuid4
+from _pytest.mark import expression
 
 import click
 from flask import current_app, g
@@ -45,6 +47,100 @@ def init_app(app):
     app.cli.add_command(init_db_command)
 
 
+# SQL helpers
+
+
+def _create(table, params: dict):
+    keys = params.keys()
+    values = params.values()
+    db = get_db()
+    db.execute(
+        f"INSERT INTO {table} ({', '.join(keys)}) VALUES ({', '.join(['?' for _ in keys])})",
+        tuple(values),
+    )
+    db.commit()
+    current_app.logger.info(f"DB: table {table}, create row {params}")
+
+
+def _read(table, project: list, query: dict):
+    keys = query.keys()
+    values = query.values()
+    row = (
+        get_db()
+        .execute(
+            f"SELECT {', '.join(project)} FROM {table} WHERE {', '.join([f'{key} = ?' for key in keys])}",
+            tuple(values),
+        )
+        .fetchone()
+    )
+    current_app.logger.info(f"DB: table {table}, read row where {query}")
+    return row
+
+
+def _read_all(table, project: list, order_by=None):
+    rows = (
+        get_db()
+        .execute(
+            f"SELECT {', '.join(project)} FROM {table}"
+            + (f" ORDER BY {order_by}" if order_by else "")
+        )
+        .fetchall()
+    )
+    current_app.logger.info(
+        f"DB: table {table}, read all rows"
+        + (f", order by {order_by}" if order_by else "")
+    )
+    return rows
+
+
+def _read_bulk(table, project: list, query: dict, order_by=None):
+    keys = query.keys()
+    values = query.values()
+    rows = (
+        get_db()
+        .execute(
+            f"SELECT {', '.join(project)} FROM {table} WHERE {', '.join([f'{key} = ?' for key in keys])}"
+            + (f" ORDER BY {order_by}" if order_by else ""),
+            tuple(values),
+        )
+        .fetchall()
+    )
+    current_app.logger.info(
+        f"DB: table {table}, read rows in bulk where {query}"
+        + (f", order by {order_by}" if order_by else "")
+    )
+    return rows
+
+
+def _update(table, query: dict, updates: dict):
+    query_keys = query.keys()
+    query_values = list(query.values())
+    update_keys = updates.keys()
+    update_values = list(updates.values())
+    db = get_db()
+    db.execute(
+        f"UPDATE {table} SET {', '.join([f'{key} = ?' for key in update_keys])} WHERE {', '.join([f'{key} = ?' for key in query_keys])}",
+        tuple(update_values + query_values),
+    )
+    db.commit()
+    current_app.logger.info(
+        f"DB: table {table}, update row {query} with values {updates}"
+    )
+
+
+def _delete(table, query: dict):
+    keys = query.keys()
+    print(f"DELETE FROM {table} WHERE {', '.join([f'{key} = ?' for key in keys])}")
+    values = query.values()
+    db = get_db()
+    db.execute(
+        f"DELETE FROM {table} WHERE {', '.join([f'{key} = ?' for key in keys])}",
+        tuple(values),
+    )
+    db.commit()
+    current_app.logger.info(f"DB: table {table}, delete row where {query}")
+
+
 # Model
 
 
@@ -53,13 +149,11 @@ def register_product(name, description):
     product_id = str(uuid4())
 
     try:
-        db = get_db()
-        db.execute(
-            "INSERT INTO products (id, name, description) VALUES (?, ?, ?)",
-            (product_id, name, description),
+        _create(
+            "products", {"id": product_id, "name": name, "description": description}
         )
-        db.commit()
-    except sqlite3.IntegrityError:
+    except sqlite3.IntegrityError as e:
+        current_app.logger.error(e)
         error = f'Name "{name}" is already used by another product.'
     except Exception as e:
         current_app.logger.error(e)
@@ -75,11 +169,7 @@ def get_all_products():
     error, products = None, None
 
     try:
-        products = (
-            get_db()
-            .execute("SELECT id, name, description FROM products ORDER BY name")
-            .fetchall()
-        )
+        products = _read_all("products", ["id", "name", "description"], order_by="name")
 
         if products is None:
             error = "Error retrieving products."
@@ -94,14 +184,7 @@ def get_product(id):
     error, product = None, None
 
     try:
-        product = (
-            get_db()
-            .execute(
-                "SELECT id, name, description FROM products WHERE id = ?",
-                (id,),
-            )
-            .fetchone()
-        )
+        product = _read("products", ["id", "name", "description"], {"id": id})
 
         if product is None:
             error = f"Product id {id} does not exist"
@@ -116,13 +199,9 @@ def update_product(id, name, description):
     error = None
 
     try:
-        db = get_db()
-        db.execute(
-            "UPDATE products SET name = ?, description = ? WHERE id = ?",
-            (name, description, id),
-        )
-        db.commit()
+        _update("products", {"id": id}, {"name": name, "description": description})
     except sqlite3.IntegrityError as e:
+        current_app.logger.error(e)
         error = f'Name "{name}" is already used by another product.'
     except Exception as e:
         current_app.logger.error(e)
@@ -135,9 +214,7 @@ def delete_product(id):
     error = None
 
     try:
-        db = get_db()
-        db.execute("DELETE FROM products WHERE id = ?", (id,))
-        db.commit()
+        _delete("products", {"id": id})
     except Exception as e:
         current_app.logger.error(e)
         error = constants.GENERIC_ERROR_MESSAGE
@@ -149,13 +226,10 @@ def get_product_offers(product_id):
     error, product_offers = None, None
 
     try:
-        product_offers = (
-            get_db()
-            .execute(
-                "SELECT id, product_id, price, items_in_stock FROM offers WHERE product_id = ?",
-                (product_id,),
-            )
-            .fetchall()
+        product_offers = _read_bulk(
+            "offers",
+            ["id", "product_id", "price", "items_in_stock"],
+            {"product_id": product_id},
         )
 
         if product_offers is None:
@@ -171,12 +245,7 @@ def add_offer(id, product_id, price, items_in_stock):
     error = None
 
     try:
-        db = get_db()
-        db.execute(
-            "INSERT INTO offers (id, product_id, price, items_in_stock) VALUES (?, ?, ?, ?)",
-            (id, product_id, price, items_in_stock),
-        )
-        db.commit()
+        _create("offers", {"id": id, "product_id": product_id, "price": price, "items_in_stock": items_in_stock})
     except Exception as e:
         current_app.logger.error(e)
         error = constants.GENERIC_ERROR_MESSAGE
@@ -191,12 +260,7 @@ def update_offer(id, product_id, price, items_in_stock):
     error = None
 
     try:
-        db = get_db()
-        db.execute(
-            "UPDATE offers SET product_id = ?, price = ?, items_in_stock = ? WHERE id = ?",
-            (product_id, price, items_in_stock, id),
-        )
-        db.commit()
+        _update("offers", {"id": id}, {"product_id": product_id, "price": price, "items_in_stock": items_in_stock})
     except Exception as e:
         current_app.logger.error(e)
         error = constants.GENERIC_ERROR_MESSAGE
@@ -208,9 +272,7 @@ def delete_offer(id):
     error = None
 
     try:
-        db = get_db()
-        db.execute("DELETE FROM offers WHERE id = ?", (id,))
-        db.commit()
+        _delete("offers", {"id": id})
     except Exception as e:
         current_app.logger.error(e)
         error = constants.GENERIC_ERROR_MESSAGE
